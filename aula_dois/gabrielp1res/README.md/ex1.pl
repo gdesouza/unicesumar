1a83f46283455fe0f720bb89addbb40ad8e18563
gabrielp1res <gabrielpires97@hotmial.com.br>
2016-08-02 22:43:54 -0300

    ex

---
+++ aula_dois/gabrielp1res/README.md/ex1.pl
@@ -0,0 +1,5 @@
+use 5.8.8;
+use strict;
+use warnings;
+	
+	print "Qual é o seu nome?"
---
+++ aula_dois/gabrielp1res/README.md/ex2.pl
@@ -0,0 +1,7 @@
+use 5.8.8;
+use strict;
+use warnings;
+my $a=0,$b=5;
+ print "A soma do numero é",$a+$b;
+ 
+ 
